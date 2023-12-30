7ff509053c9bdc9f4363ebf337d309fb3d011a3d
camdentadhg <camdent@gmail.com>
2023-12-29 19:17:03 -0600

    question pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import Question, Survey, satisfaction_survey, personality_quiz, surveys
 
@@ -14,19 +14,24 @@ def show_start():
     """Show simple start page for survey"""
     return render_template("start.html", current_survey = satisfaction_survey)
 
+@app.route('/questions/<number>')
+def show_question(number):
+    """Show the appropriately number question and answer options"""
+    number = int(number)
+    return render_template("question.html", current_survey = satisfaction_survey, number = number)
 
 
 
-# 19 create start page and style it
-# 18 create question route
-# 17 create question page and style it
-# 16 handle incoming form data (POST)
-# 15 create thank you route 
-# 14 redirect to thank you when questions are complete
-# 13 create thank you page and style it
-# 12 change view function to check that it is the right quuestion page and redirect as necessary
-# 11 when all questions have been answered, any page should redirect to thank you page
-# 10 use flash message to tell user they are trying to access an invalid question when they try to go out of order
+# 18 create question page and style it
+# 17 handle incoming form data (POST)
+# 16 create thank you route 
+# 15 redirect to thank you when questions are complete
+# 14 create thank you page and style it
+# 13 change view function to check that it is the right quuestion page and redirect as necessary
+# 12 when all questions have been answered, any page should redirect to thank you page
+# 11 use flash message to tell user they are trying to access an invalid question when they try to go out of order
+
+# 10 store the answers in a session
 # 9 make the system able to handle more than one survey
 # 8 make a new survey so that there are three
 # 7 allow comments for some questions
@@ -35,6 +40,6 @@ def show_start():
 # 4 add Bootstrap to the site
 # 3 allow users to skip questions
 # 2 allow users to go back to previously answered questions and change the answer
-# 1 make a web interface to allow users to create surveys through the web and adding them to the system
+# 1 make a web interface to allow users to create surveys through the web and add them to the system
 
 # December 30th, 2023 - 
