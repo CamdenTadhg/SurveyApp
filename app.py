777b84be16e4d71e648742fef13a94a5a5fc86f6
camdentadhg <camdent@gmail.com>
2023-12-29 22:23:46 -0600

    basic functionality completed

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import Question, Survey, satisfaction_survey, personality_quiz, surveys
+from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "requin"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -18,6 +19,13 @@ def show_start():
 def show_question(number):
     """Show the appropriately number question and answer options"""
     number = int(number)
+    if len(responses) == len(satisfaction_survey.questions):
+        flash("You have already completed this survey! Thank you.", 'error')
+        return redirect('/thankyou')
+    if number != len(responses):
+        next_number = len(responses)
+        flash("You are trying to access an invalid question. Please answer this question.", 'error')
+        return redirect(f'/questions/{next_number}') 
     return render_template("question.html", current_survey = satisfaction_survey, number = number)
 
 @app.route('/answer', methods=["POST"])
@@ -35,13 +43,6 @@ def collect_answer():
 def show_thank_you():
     """Show thank you message upon survey completion"""
     return render_template("thankyou.html", current_survey = satisfaction_survey)
-
-
-
-# 4 create thank you page and style it
-# 3 change view function to check that it is the right quuestion page and redirect as necessary
-# 2 when all questions have been answered, any page should redirect to thank you page
-# 1 use flash message to tell user they are trying to access an invalid question when they try to go out of order
 
 # 10 store the answers in a session
 # 9 make the system able to handle more than one survey
